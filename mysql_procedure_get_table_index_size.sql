01431935e8a2d4106cf63fda405a3c49aa6fe669
Michael Coburn <michael.coburn@percona.com>
2019-07-08 16:20:11 -0500

    Fixing typo

--- mysql_procedure_get_table_index_size.sql
+++ mysql_procedure_get_table_index_size.sql
@@ -3,8 +3,8 @@ delimiter //
 CREATE PROCEDURE mysql.proc_pmm_update_table_index_size ()
 BEGIN
 DECLARE inf_datalength INT;
-DECLARE pmm_datalenght INT;
-DECLARE inf_indexlenght INT;
+DECLARE pmm_datalength INT;
+DECLARE inf_indexlength INT;
 DECLARE databasename VARCHAR(64);
 DECLARE done INT DEFAULT 0;
 DECLARE databases_list CURSOR FOR SELECT DISTINCT(TABLE_SCHEMA) FROM information_schema.TABLES WHERE TABLE_SCHEMA NOT IN ('information_schema','mysql','performance_schema','sys');
@@ -26,16 +26,16 @@ END IF;
   IF done THEN
     LEAVE tables_loop;
   END IF;
-   	SELECT IFNULL(DATA_LENGTH,0),IFNULL(INDEX_LENGTH,0) INTO inf_datalength,inf_indexlenght FROM information_schema.TABLES WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;
-   	SELECT DATA_LENGTH INTO pmm_datalenght FROM mysql.pmm_custom_informations_data_index_size WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;
-   	IF(pmm_datalenght IS NULL) THEN
+   	SELECT IFNULL(DATA_LENGTH,0),IFNULL(INDEX_LENGTH,0) INTO inf_datalength,inf_indexlength FROM information_schema.TABLES WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;
+   	SELECT DATA_LENGTH INTO pmm_datalength FROM mysql.pmm_custom_informations_data_index_size WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;
+   	IF(pmm_datalength IS NULL) THEN
       SET done = 0;
-		  INSERT INTO mysql.pmm_custom_informations_data_index_size (TABLE_SCHEMA,TABLE_NAME,DATA_LENGTH, INDEX_LENGTH) VALUES (databasename,tablename,inf_datalength,inf_indexlenght);
+		  INSERT INTO mysql.pmm_custom_informations_data_index_size (TABLE_SCHEMA,TABLE_NAME,DATA_LENGTH, INDEX_LENGTH) VALUES (databasename,tablename,inf_datalength,inf_indexlength);
 		ELSE
 			IF inf_datalength != pmm_datalenght
 			THEN
 			SELECT 'Màj';
-		   UPDATE mysql.pmm_custom_informations_data_index_size SET DATA_LENGTH = inf_datalength, INDEX_LENGTH = inf_indexlenght WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;
+		   UPDATE mysql.pmm_custom_informations_data_index_size SET DATA_LENGTH = inf_datalength, INDEX_LENGTH = inf_indexlength WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;
 			END IF;
 		END IF;
 	END LOOP;
