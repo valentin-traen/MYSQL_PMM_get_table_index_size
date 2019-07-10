8d1aa3e21721c523b50cdab4dfe775a62f7edc14
Valentin <49306431+valentinmysql@users.noreply.github.com>
2019-07-10 10:45:35 +0200

    Correction when adding table and improvment
    - new tables added in pmm_custom_informations_data_index_size
    - old databases deleted from pmm_custom_informations_data_index_size
    - old tables deleted from pmm_custom_informations_data_index_size

--- mysql_procedure_get_table_index_size.sql
+++ mysql_procedure_get_table_index_size.sql
@@ -15,33 +15,72 @@ FETCH databases_list INTO databasename;
 IF done THEN
 LEAVE databases_loop;
 END IF;
-	BLOCK2: BEGIN
-  DECLARE tablename VARCHAR(64);
-	DECLARE done INT DEFAULT 0;
-	DECLARE tables_list CURSOR FOR SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = databasename;
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-	OPEN tables_list;
-	tables_loop: LOOP
-	FETCH tables_list INTO tablename;
+
+    BLOCK2: BEGIN
+  	DECLARE tablename VARCHAR(64);
+    DECLARE done INT DEFAULT 0;
+    DECLARE tables_list CURSOR FOR SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = databasename;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+    OPEN tables_list;
+    tables_loop: LOOP
+    FETCH tables_list INTO tablename;
   IF done THEN
-    LEAVE tables_loop;
+  	LEAVE tables_loop;
   END IF;
-   	SELECT IFNULL(DATA_LENGTH,0),IFNULL(INDEX_LENGTH,0) INTO inf_datalength,inf_indexlength FROM information_schema.TABLES WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;
-   	SELECT DATA_LENGTH INTO pmm_datalength FROM mysql.pmm_custom_informations_data_index_size WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;
-   	IF(pmm_datalength IS NULL) THEN
-      SET done = 0;
-		  INSERT INTO mysql.pmm_custom_informations_data_index_size (TABLE_SCHEMA,TABLE_NAME,DATA_LENGTH, INDEX_LENGTH) VALUES (databasename,tablename,inf_datalength,inf_indexlength);
-		ELSE
-			IF inf_datalength != pmm_datalength
-			THEN
-			SELECT 'Màj';
-		   UPDATE mysql.pmm_custom_informations_data_index_size SET DATA_LENGTH = inf_datalength, INDEX_LENGTH = inf_indexlength WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;
-			END IF;
-		END IF;
-	END LOOP;
-	CLOSE tables_list;
-	END BLOCK2;
+  SELECT DATA_LENGTH INTO pmm_datalength FROM mysql.pmm_custom_informations_data_index_size WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;
+  SELECT IFNULL(DATA_LENGTH,0),IFNULL(INDEX_LENGTH,0) INTO inf_datalength,inf_indexlength FROM information_schema.TABLES WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;
+  IF EXISTS (SELECT DATA_LENGTH FROM mysql.pmm_custom_informations_data_index_size WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename) THEN
+   		IF inf_datalength != pmm_datalength THEN        
+        	UPDATE mysql.pmm_custom_informations_data_index_size SET DATA_LENGTH = inf_datalength, INDEX_LENGTH = inf_indexlength WHERE BINARY TABLE_SCHEMA = databasename AND BINARY TABLE_NAME = tablename;        
+        END IF;
+  ELSE
+  	SET done = 0;
+  	INSERT INTO mysql.pmm_custom_informations_data_index_size (TABLE_SCHEMA,TABLE_NAME,DATA_LENGTH, INDEX_LENGTH) VALUES (databasename,tablename,inf_datalength,inf_indexlength);
+  END IF;
+END LOOP;
+CLOSE tables_list;
+END BLOCK2;
 END LOOP;
 CLOSE databases_list;
+
+BLOCK3: BEGIN
+DECLARE done INT DEFAULT 0;
+DECLARE pmm_databasename VARCHAR(64);
+DECLARE pmm_databases_list CURSOR FOR SELECT DISTINCT(TABLE_SCHEMA) FROM mysql.pmm_custom_informations_data_index_size;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+OPEN pmm_databases_list;
+pmm_databases_loop: LOOP
+FETCH pmm_databases_list INTO pmm_databasename;
+IF done THEN
+LEAVE pmm_databases_loop;
+END IF;
+IF NOT EXISTS (SELECT TABLE_NAME FROM information_schema.TABLES WHERE BINARY TABLE_SCHEMA = pmm_databasename) THEN
+	DELETE FROM mysql.pmm_custom_informations_data_index_size WHERE BINARY TABLE_SCHEMA = pmm_databasename;
+ELSE
+
+	BLOCK4: BEGIN
+	DECLARE done INT DEFAULT 0;
+	DECLARE pmm_tablename VARCHAR(64);
+	DECLARE pmm_tables_list CURSOR FOR SELECT DISTINCT(TABLE_NAME) FROM mysql.pmm_custom_informations_data_index_size WHERE BINARY TABLE_SCHEMA = pmm_databasename;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+	OPEN pmm_tables_list;
+	pmm_tables_loop: LOOP
+	FETCH pmm_tables_list INTO pmm_tablename;
+	IF done THEN
+	LEAVE pmm_tables_loop;
+	END IF;
+	IF NOT EXISTS (SELECT DISTINCT(TABLE_NAME) FROM information_schema.TABLES WHERE BINARY TABLE_SCHEMA = pmm_databasename AND BINARY TABLE_NAME = pmm_tablename) THEN
+	DELETE FROM mysql.pmm_custom_informations_data_index_size WHERE BINARY TABLE_SCHEMA = pmm_databasename AND BINARY TABLE_NAME = pmm_tablename;
+	END IF;
+	END LOOP;
+	CLOSE pmm_tables_list;
+	END BLOCK4;
+
+END IF;
+END LOOP;
+CLOSE pmm_databases_list;
+END BLOCK3;
+
+
 END //
-delimiter ;
+DELIMITER ;
